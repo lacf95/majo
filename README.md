cc718f10a29dc78baec01baf263fcab0cd9ef367
Luis Adrián Chávez Fregoso <biolacf@gmail.com>
2020-07-30 16:54:32 -0500

    QR Generator template application

---
+++ README.md
@@ -0,0 +1,4 @@
+# Majo's exercises
+
+## QR Generator
+Create a QR Generator using the [QrTag](https://www.qrtag.net/api/) API
---
+++ qr_generator/js/qr-generator.js
@@ -0,0 +1,9 @@
+function QRGenerator(url, size, format) {
+  this.generatorUriTemplate = function generatorUriTemplate(options) {
+    return;
+  };
+
+  this.generate = function generate() {
+    return this.generatorUriTemplate();
+  };
+}
