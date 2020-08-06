b610bae7a928e6f36123fb38514372f014809f09
Luis Adrián Chávez Fregoso <biolacf@gmail.com>
2020-08-06 14:52:44 -0500

    Adds League of Legends Champedia

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## QR Generator
 Create a QR Generator using the [QrTag](https://www.qrtag.net/api/) API
+
+## League of Legends Champedia
+Create a Wiki for all the [League of Legends](https://developer.riotgames.com/docs/lol) champions using the official API
---
+++ lol_champedia/js/league-of-legends.js
@@ -0,0 +1,14 @@
+function LeagueOfLegends(lang) {
+  // Champions list
+  // http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion.json
+
+  this.search = function search(query, subject = 'name') {
+  }
+
+  // Aatrox big image can be found at
+  // http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg
+  // Thumbnail image can be found at
+  // http://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/Aatrox.png
+  this.champDetails = function champDetails(id) {
+  }
+}
